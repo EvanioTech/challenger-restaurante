8c0870fbe4e43e251069489ae1b7e02ff404928f
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2024-10-22 10:53:05 -0300

    app finish

--- app-chall/App.js
+++ app-chall/App.js
@@ -2,23 +2,61 @@ import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 
+
+let clientes = 0;
+
+
 export default function App() {
-  const [numero,setNumero] = useState(0)
-function adicionar(){
+  const [numero,setNumero] = useState(0);
+  const[cheio,setCheio] = useState('');
+
+function Adicionar(){
+  if(clientes <10){
+    clientes++;  
+    
+  }
+  if(clientes > 9){
+    aviso()
+  }
   
   
+  
+  setNumero(clientes)
+  
+  
+};
+function aviso(){
+  
+    
+  setCheio('Restaurante está no seu limite de pessoas.')
+  
+ 
+};
+function Remover(){
+  if(clientes >=1){
+    clientes--
+  }
+  if (clientes <10) {
+    setCheio('')
+  }
+  
+  else{console.log('aki')}
+  
+  setNumero(clientes)
 }
   return (
     <View style={styles.container}>
-      <Text>Pessoas no restaurante:</Text>
-      <Text style >{numero}</Text>
+      <Text style ={{fontSize:25}}>Pessoas no restaurante:</Text>
+
+      <View style={{backgroundColor: 'black', margin:20, width:50, borderRadius:10,alignItems: 'center'}}>
+      <Text style={{color: 'white', fontSize:44}} >{numero}</Text>
+      </View>
+      <Text style={{backgroundColor: 'orange'}} onPress={aviso} >{cheio}</Text>
       <View style={styles.areaView}>
-      <TouchableOpacity style={styles.btnArea}>
-        <Text style={styles.btnTexto} onPress={adicionar}>
-          Adicionar
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnArea}>
+      <TouchableOpacity style={[styles.btnArea, { backgroundColor: numero >= 10 ? 'gray' : '#1c9ff9' }]} onPress={Adicionar} disabled={numero >= 10}>
+          <Text style={styles.btnTexto}>Adicionar</Text>
+        </TouchableOpacity>
+      <TouchableOpacity style={[styles.btnArea , { backgroundColor: numero < 1 ? 'gray' : '#1c9ff9' }]} onPress={Remover} disabled={numero < 1}>
         <Text style={styles.btnTexto}>
           Remover
         </Text>
